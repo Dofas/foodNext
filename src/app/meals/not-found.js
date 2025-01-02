730a6f281dcdfae068d8b28f1c386f7d7a16dac8
Yevhen Liuklian <yevhen.liuklian@healthbookplus.com>
2025-01-02 21:58:14 +0200

    2nd commit

---
+++ src/app/meals/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <main className="not-found">
+            <h1>Meal not found</h1>
+            <p>We could not find the requested page</p>
+        </main>
+    );
+};
+
+export default NotFound;
--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,8 +1,17 @@
 import classes from './page.module.css';
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
+import {getMeals} from "@/lib/meals";
+import {Suspense} from "react";
+import Loader from './loading-out';
+
+async function Meals() {
+    const meals = await getMeals();
+    return  <MealsGrid meals={meals}/>;
+}
 
 export default function MealsPage() {
+
     return <>
         <header className={classes.header}>
             <h1>Delicious meals, created <span className={classes.highlight}>by you</span></h1>
@@ -13,8 +22,10 @@ export default function MealsPage() {
                 </Link>
             </p>
         </header>
-        <main className={classes.main}>
-            <MealsGrid meals={[]}/>
+        <main>
+            <Suspense fallback={<Loader/>}>
+                <Meals />
+            </Suspense>
         </main>
     </>
 }
